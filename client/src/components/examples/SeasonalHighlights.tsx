8c42ae1d3313009b7a2c87bb4fef135eed23a869
Souvik Sengupta <147796300+S0UVIK12@users.noreply.github.com>
2025-09-16 00:40:41 +0530

    Delete client directory

--- client/src/components/examples/SeasonalHighlights.tsx
+++
@@ -1,9 +0,0 @@
-import SeasonalHighlights from '../SeasonalHighlights';
-
-export default function SeasonalHighlightsExample() {
-  return (
-    <SeasonalHighlights
-      onExploreAttraction={(season) => console.log('Explore attraction for season:', season)}
-    />
-  );
-}
